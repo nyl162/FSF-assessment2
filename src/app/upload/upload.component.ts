3b2c9564bcf84c2c79b9501ba1715b024f8e4146
NGYL <nyl162@hotmail.com>
2018-09-28 16:56:23 +0800

    .

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit} from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { FormsModule, NgForm, FormControl,FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { BookService } from '../service/book.service';
 import { environment } from '../../environments/environment';
 
@@ -10,9 +10,52 @@ import { environment } from '../../environments/environment';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading: boolean = false;
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      avatar: null
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onFileChange(event) {
+    if(event.target.files.length > 0) {
+      let file = event.target.files[0];
+      this.form.get('avatar').setValue(file);
+    }
+  }
+
+  private prepareSave(): any {
+    let input = new FormData();
+    input.append('avatar', this.form.get('avatar').value);
+    return input;
+  }
+
+  onSubmit() {
+    const formModel = this.prepareSave();
+    this.loading = true;
+    // In a real-world app you'd have a http request / service call here like
+    // this.http.post('apiUrl', formModel)
+    setTimeout(() => {
+      // FormData cannot be inspected (see "Key difference"), hence no need to log it here
+      alert('done!');
+      this.loading = false;
+    }, 1000);
+  }
+
+  clearFile() {
+    this.form.get('avatar').setValue(null);
+    this.fileInput.nativeElement.value = '';
   }
 
 }
